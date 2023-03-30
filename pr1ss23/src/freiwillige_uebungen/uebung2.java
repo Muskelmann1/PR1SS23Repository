e3ec4521f8b56083f4cbbc51f412e85321724024
bobla <bobla@141.19.147.237>
2023-03-30 13:56:50 +0200

    freiwillige uebung

---
+++ pr1ss23/src/freiwillige_uebungen/uebung2.java
@@ -0,0 +1,24 @@
+package freiwillige_uebungen;
+import java.util.Scanner;
+
+public class uebung2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		char [] chars = new char[10];
+		char I = 'I';
+		
+		for(int i = 0; i<input.length(); i++) {
+			char currentChar = input.charAt(i);
+			if(currentChar.equals(I)) {
+				System.out.println("correct!");
+			}
+			else {
+				System.out.println("Invalid Input!");
+			}
+
+		}
+	}
+
+}
